3ea722ef339632e2a66a2dc721a045b80d24c672
Tyler Rocha <tyler.rocha@utexas.edu>
2017-03-16 00:02:13 +0800

    compute raw term weights for query vector

--- HW3/search.py
+++ HW3/search.py
@@ -6,6 +6,8 @@ import query_parser
 import boolops
 from utils import normalize_token
 from nltk.tokenize import word_tokenize
+from math import log
+import heapq
 
 def search(dict_file, postings_file, queries_file, results_file):
     dictionary = Dictionary.load_dict_from_file(dict_file)
@@ -21,16 +23,15 @@ def search(dict_file, postings_file, queries_file, results_file):
         results_f.close()
     query_f.close()
 
-def get_normal_tokens(query):
+def get_normal_terms(query):
     query = query.strip()
-    query = normalize_token(query)
-    tokens = word_tokenize(query)
-    normal_tokens = []
-    for token in tokens:
-        token = normalize_token(token)
-        token = token.encode('ascii') #removes 'u' from print
-        normal_tokens.append(token)
-    return normal_tokens
+    terms = word_tokenize(query)
+    normal_terms = []
+    for term in terms:
+        term = normalize_token(term)
+        term = term.encode('ascii') #removes 'u' from print
+        normal_terms.append(term)
+    return normal_terms
 
 # for every term, docID pair,
 # tf_list maps docIDs to their tf
@@ -47,13 +48,37 @@ def compute_term_frequency(term, dict, postings_file, tf_list):
         print term, "not in dict"
     return []
 
+# def compute_tf_idf(term, dict, postings_file):
+#     tf = compute_tf(term, dict)
+
+# count occurences of each term in query and assign raw tf weight
+def get_tf_raw_for_query(terms):
+    vector = {}
+    for term in terms:
+        if term not in vector:
+            vector[term] = 1
+        else:
+            vector[term] = vector[term] + 1
+    print "get_tf_raw_for_query", vector
+    return vector
+
+def create_query_vector(terms, dict, postings_file):
+    vector = get_tf_raw_for_query(terms)
+    # for term in terms:
+    #     tf_idf = compute_tf_idf(term, dict, postings_file)
+    #     print "tf_idf =", tf_idf
+    #     vector[term] = tf_idf
+    print vector
+
+
 def process_query(query, dict, postings_file):
     with PostingFile(postings_file, 'r') as postings_file:
-
-        tokens = get_normal_tokens(query)
-        print query, "->", tokens
+        heap = []
+        terms = get_normal_terms(query)
+        query_vector = create_query_vector(terms, dict, postings_file)
+        print query.strip(), "->", terms
         tf_list = []
-        for term in tokens:
+        for term in terms:
             tf_list = compute_term_frequency(term, dict, postings_file, tf_list)
 
     postings_file.close()
