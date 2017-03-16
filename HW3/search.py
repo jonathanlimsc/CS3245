bad9ee638f874c30bea02debe544b6b158756158
Tyler Rocha <tyler.rocha@utexas.edu>
2017-03-16 13:40:15 +0800

    add get_doc_freq function

--- HW3/search.py
+++ HW3/search.py
@@ -35,22 +35,34 @@ def get_normal_terms(query):
 
 # for every term, docID pair,
 # tf_list maps docIDs to their tf
-def compute_term_frequency(term, dict, postings_file, tf_list):
-    p_list = []
-    if dict.has_term(term):
-        start_ptr = dict.start_ptr_hash[term]
+def create_document_vector(term, dictionary, postings_file, document_vectors, query_vector):
+    p_list = query_vector.keys()
+
+    if dictionary.has_term(term):
+        start_ptr = dictionary.start_ptr_hash[term]
         p_list = postings_file.read_posting_entry(start_ptr)
         print "read_posting_entry", p_list.doc_id, p_list.term_freq, p_list.next_ptr
         while p_list.next_ptr != -1:
             p_list = postings_file.read_posting_entry(p_list.next_ptr)
             print "\t", p_list.doc_id, p_list.term_freq, p_list.next_ptr
     else:
-        print term, "not in dict"
-    return []
+        print term, "not in dictionary"
+    return p_list
+
+# if present, returns the document frequency for a term in the dictionary, 1 otherwise
+def get_doc_freq(term):
+    doc_freq = 0
+    if dictionary.has_term(term):
+        doc_freq = dictionary.doc_freq_hash[term]
+        print doc_freq
+    else:
+        doc_freq = 1
+        print term, "not in dictionary, how to compute?"
+    return doc_freq
 
 # count occurences of each term in query and use as raw_weight
 # then take log(raw_weight) + 1 and use as final weight
-def create_query_vector(terms):
+def create_query_vector(terms, dictionary, postings_file):
     vector = {}
     for term in terms:
         if term not in vector:
@@ -60,22 +72,30 @@ def create_query_vector(terms):
     print "raw tf for query", vector
     for term in vector:
         vector[term] = math.log(vector[term], 10) + 1
+
+        doc_freq = get_doc_freq(term)
+
+
+        total_docs = len(dictionary.doc_ids)
+        print total_docs
+        vector[term] *= math.log(total_docs, doc_freq)
     print "W(t,q) for query", vector
     return vector
 
-def process_query(query, dict, postings_file):
+def process_query(query, dictionary, postings_file):
     with PostingFile(postings_file, 'r') as postings_file:
         heap = []
         terms = get_normal_terms(query)
-        query_vector = create_query_vector(terms)
-        #print query.strip(), "->", terms
+        query_vector = create_query_vector(terms, dictionary, postings_file)
         document_vectors = []
+
         for term in terms:
-            tf_list = compute_term_frequency(term, dict, postings_file, tf_list)
+            new_vector = create_document_vector(term, dictionary, postings_file, document_vectors, query_vector)
+            document_vectors.add(new_vector)
 
     postings_file.close()
 
-    return []#result
+    return heap
 
 def usage():
     print "Usage: " + sys.argv[0] + " -d <dictionary-file> -p <postings-file> -q <queries-file> -o <results-file>"
